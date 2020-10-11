9a1ecd273699a5197a9fe52dc396587fb2d6e118
Felix ItsTheIntrovertedGuy <max0018@t-online.de>
2020-10-11 17:01:18 +0200

    Linear memory allocator, because I catch myself using that more often

--- language_layer.h
+++ language_layer.h
@@ -33,6 +33,46 @@ typedef   double f64;
 #define SIGN(a)  ((a) >  0  ?  1  :  ((a) < 0 ? -1 : 0))
 #define CLAMP(a, min, max) MIN((max), MAX((min), (a)))
 
+typedef struct
+{
+	u8 *Memory;
+	u64 BlockSize;
+	u64 BytesAllocated;
+} memory_block;
+
+#ifdef __linux__
+#include <sys/mman.h>
+#endif
+
+internal memory_block
+CreateLinearAllocator(u64 MemoryBlockSize)
+{
+	memory_block Result = { 0 };
+	Result.BlockSize = MemoryBlockSize;
+#ifdef __linux__
+	Result.Memory = mmap(0, Result.BlockSize, PROT_READ|PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);;
+#endif
+	return (Result);
+}
+
+internal void *
+AllocateMemory(memory_block *Memory, u64 BytesToAllocate)
+{
+	Assert(Memory->BlockSize > Memory->BytesAllocated + BytesToAllocate);
+	void *MemoryResult = Memory->Memory + Memory->BytesAllocated;
+	Memory->BytesAllocated += BytesToAllocate;
+	return (MemoryResult);
+}
+
+internal void
+FreeMemoryBlock(memory_block *Memory)
+{
+#ifdef __linux__
+	munmap(Memory->Memory, Memory->BlockSize);
+#endif
+	*Memory = (memory_block) { 0 };
+}
+
 internal u64
 U64ChangeEndianess(u64 Value)
 {
@@ -121,6 +161,14 @@ StringEqual(char *A, char *B)
 	}
 	
 	return (A[Index] == B[Index]);
+}
+
+internal void
+StringAppend(char *Destination, char *ToAppend)
+{
+	while (*Destination != 0) { Destination++; }
+	while (*ToAppend != 0) { *(Destination++) = *(ToAppend++); }
+	*Destination = 0;
 }
 
 internal b32
@@ -389,3 +437,5 @@ MemoryClear(void *Destination, u64 BytesToClear)
 		Buffer[ByteIndex] = 0;
 	}
 }
+
+
